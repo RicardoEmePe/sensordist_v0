a31236681c407be36c8b4421ed0fe4cc85f05247
RICARDO MARQUINA <37264449+RicardoEmePe@users.noreply.github.com>
2024-04-20 16:23:41 +0200

    Actualització main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,13 @@
+let distancia = 0
 basic.forever(function () {
-    basic.showString("" + (sonar.ping(
+    distancia = sonar.ping(
     DigitalPin.P1,
     DigitalPin.P0,
     PingUnit.Centimeters
-    )))
+    )
+    basic.showNumber(distancia)
+    basic.pause(100)
+    if (distancia < 20) {
+        basic.showIcon(IconNames.Heart)
+    }
 })
